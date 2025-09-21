87cff32acaffd1a05aab111c082bfd99925ebbc4
riennothing <137658714+riennothing@users.noreply.github.com>
2025-09-21 19:10:37 -0400

    Create ConcatTableaux.h

---
+++ Tableau_Dynamiques/ConcatTableaux.h
@@ -0,0 +1,12 @@
+#ifndef CONCATTABLEAUX_H
+#define CONCATTABLEAUX_H
+
+int* concatenerTableaux(const int* tab1, size_t taille1, const int* tab2, size_t taille2);
+
+#endif
+
+/* Travail dans ConcatTableaux.cpp :
+- Créer un nouveau tableau de taille taille1 + taille2
+- Copier les éléments des deux tableaux
+- Retourner le nouveau tableau
+*/
