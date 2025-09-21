4852f006ec5e711236d74b37f38a08d323d90a0c
riennothing <137658714+riennothing@users.noreply.github.com>
2025-09-21 19:17:47 -0400

    Create RechercheBinaire.h

---
+++ Tableaux_Dynamiques/RechercheBinaire.h
@@ -0,0 +1,12 @@
+#ifndef RECHERCHEBINAIRE_H
+#define RECHERCHEBINAIRE_H
+
+int rechercheBinaire(const int* tableau, size_t taille, int valeur);
+
+#endif
+
+/* Travail dans RechercheBinaire.cpp :
+- Implémenter la recherche binaire
+- Tableau doit être trié
+- Retourner l'indice ou -1 si non trouvé
+*/
