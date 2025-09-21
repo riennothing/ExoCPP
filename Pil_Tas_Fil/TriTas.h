25d77a7492d1d50d9344054d8b8a74c4e824bbaf
riennothing <137658714+riennothing@users.noreply.github.com>
2025-09-21 19:54:55 -0400

    Create TriTas.h

---
+++ Pil_Tas_Fil/TriTas.h
@@ -0,0 +1,15 @@
+#ifndef TRITAS_H
+#define TRITAS_H
+
+void triTas(int* tableau, size_t taille);
+void construireTas(int* tableau, size_t taille);
+void tamiser(int* tableau, size_t taille, size_t racine);
+
+#endif
+
+/* Travail dans TriTas.cpp :
+- Implémenter l'algorithme Heapsort
+- Complexité O(n log n) dans tous les cas
+- Trier sur place sans mémoire supplémentaire
+- Tester avec différents tableaux
+*/
