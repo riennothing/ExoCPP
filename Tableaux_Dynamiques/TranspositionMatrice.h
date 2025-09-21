d55b7ab1371d267222b88443c6033f32121191bd
riennothing <137658714+riennothing@users.noreply.github.com>
2025-09-21 19:24:22 -0400

    Create TranspositionMatrice.h

---
+++ Tableaux_Dynamiques/TranspositionMatrice.h
@@ -0,0 +1,12 @@
+#ifndef TRANSPOSITIONMATRICE_H
+#define TRANSPOSITIONMATRICE_H
+
+int** transposerMatrice(const int** matrice, size_t lignes, size_t colonnes);
+
+#endif
+
+/* Travail dans TranspositionMatrice.cpp :
+- Créer nouvelle matrice de dimensions inversées
+- Copier avec transposition
+- Retourner la matrice transposée
+*/
