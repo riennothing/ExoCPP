ca20193aea9a7dc3761fe9ec6b205c5346d1b438
riennothing <137658714+riennothing@users.noreply.github.com>
2025-09-21 19:23:34 -0400

    Create ProduitMatriciel.h

---
+++ Tableaux_Dynamiques/ProduitMatriciel.h
@@ -0,0 +1,12 @@
+#ifndef PRODUITMATRICIEL_H
+#define PRODUITMATRICIEL_H
+
+int** produitMatriciel(const int** matrice1, size_t l1, size_t c1, const int** matrice2, size_t l2, size_t c2);
+
+#endif
+
+/* Travail dans ProduitMatriciel.cpp :
+- Vérifier compatibilité des dimensions (c1 == l2)
+- Calculer le produit matriciel
+- Retourner la nouvelle matrice
+*/
