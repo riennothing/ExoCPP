ae417e8da9c71d18067fdf0baa6280f17a65369f
riennothing <137658714+riennothing@users.noreply.github.com>
2025-09-21 19:16:45 -0400

    Create TriBulle.h

---
+++ Tableaux_Dynamiques/TriBulle.h
@@ -0,0 +1,12 @@
+#ifndef TRIBULLE_H
+#define TRIBULLE_H
+
+void triBulle(int* tableau, size_t taille);
+
+#endif
+
+/* Travail dans TriBulle.cpp :
+- Implémenter l'algorithme de tri à bulles
+- Échanger les éléments si nécessaire
+- Optimiser avec un flag d'échange
+*/
